5e375a53f4c0bf8ad92d2e0336fed293173fa615
unknown <abhisheksrivastava9719@gmail.com>
2022-08-26 09:25:44 +0530

    Main file uploaded

---
+++ RepositoryLayer/Services/FunDoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Services.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Services
+{
+    public class FunDoContext:DbContext
+    {
+        public FunDoContext(DbContextOptions option) : base(option)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
